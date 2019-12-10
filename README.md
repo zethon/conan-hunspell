1badbd3040cb264a52e510f473e44029635c4ce9
Adalid Claure <adalid.claure@rsa.com>
2019-12-10 13:52:51 -0500

    updating the hunspell recipe

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # conan-hunspell
 
-Conan packages for Hunspell spell checker
+[Conan](https://bintray.com/zethon/owl/libhunspell%3Aowl) package for the [Hunspell](https://github.com/hunspell/hunspell.git) spell-checker and morphological analyzer library.
 
-* checkout the repo
-* cd into the repo
-* `conan source . -sf source`
-* `conan build . -sf source`
-* `conan package . -pf package`
+## Usage
 
+Add `libhunspell/x.y.z@owl/stable` in the list of requirements of your conanfile, where `x.y.z` is the desired version. See [how to use a conanfile.py](http://docs.conan.io/en/latest/mastering/conanfile_py.html) for more information.
+
+## Packaging
+
+See [Conan-Package-Tools](https://github.com/conan-io/conan-package-tools) Github page for more information on packaging.
--- conanfile.py
+++ conanfile.py
@@ -14,39 +14,31 @@ class LibHunspellConan(ConanFile):
     options = {"shared": [True,False]}
     default_options = "shared=False"
 
-    scm = {
-        "type": "git",
-        "url": "https://github.com/hunspell/hunspell.git",
-        "revision": "89d1084f1760c2cdbe1b636eb1cdc15e6f3ac519" # 1.6.2 + fixes
-    }
+    scm = {"revision": "89d1084f1760c2cdbe1b636eb1cdc15e6f3ac519",
+           "type": "git",
+           "url": "https://github.com/hunspell/hunspell.git"}
 
     def source(self):
         archive_name = "hunspell-%s.zip" % self.version
         url = "https://github.com/hunspell/hunspell/archive/v%s.zip" % self.version
-        self.output.info("Downloading %s to..." % url)
-        # self.output.info("Saving to %s ..." % archive_name)
         tools.download(url, archive_name)
         tools.unzip(archive_name)
         os.unlink(archive_name)
         os.rename("hunspell-%s" % self.version, "source")
-        
 
     def build(self):
         if self.settings.compiler == "Visual Studio":
-            replace_in_file("msvc\\libhunspell.vcxproj", "v140_xp", "v140")
             msbuild = MSBuild(self)
             build_type = None
             if self.options.shared:
                 build_type = {"Release":"Release_dll", "Debug":"Debug_dll"}[str(self.settings.build_type)]
             msbuild.build("msvc\\Hunspell.sln", targets=["libhunspell"], build_type=build_type, platforms={"x86":"Win32"})
-        elif self.settings.compiler == "apple-clang":
+        else:
             with tools.chdir("source"):
                 self.run("autoreconf --verbose --install --force")
                 env_build = AutoToolsBuildEnvironment(self)
                 env_build.configure()
-                env_build.make()
-        else:
-            raise str("Compiler " + str(self.settings.compiler) + " not supported")
+                env_build.make(args=["-j4"])
 
     def package(self):
         if conans.tools.os_info.is_windows:
@@ -57,14 +49,14 @@ class LibHunspellConan(ConanFile):
             self.copy("*libhunspell.so*", dst="bin", keep_path=False)
             self.copy("*libhunspell.lib", dst="lib", keep_path=False)
             self.copy("*libhunspell.a", dst="lib", keep_path=False)
-        elif conans.tools.os_info.is_macos:
+        else:
             for h in ["hunspell.hxx", "hunspell.h", "hunvisapi.h", "w_char.hxx", "atypes.hxx"]:
                 self.copy(h, dst="include/hunspell", src="src/hunspell")            
-            self.copy("*libhunspell-1.6.a", dst="lib", keep_path=False)
+            self.copy("*libhunspell-1.7.a", dst="lib", keep_path=False)
             self.copy("*libhunspell.a", dst="lib", keep_path=False)
         
     def package_info(self):
         if self.settings.compiler == "Visual Studio":
             self.cpp_info.libs = ["libhunspell.lib"]
         else:
-            self.cpp_info.libs = ["hunspell-1.6"]
+            self.cpp_info.libs = ["hunspell-1.7"]
